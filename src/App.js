d58ac0abf4bbd3cc0b2fbfc6fe40a468150d9472
Soumik Seth <91617951+soumikseth@users.noreply.github.com>
2022-09-20 16:18:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Login from "./components/login/login";
+import Signup from "./components/signUp/signUp";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import PropertyView from "./components/PropertyView/PropertyView";
+
+import BasicInfo from "./BasicInfo";
+import PropertyDetail from "./PropertyDetail";
+import GeneralInfo from "./GeneralInfo";
+import LocationInfo from "./LocationInfo";
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/basic-info" element={<BasicInfo />} />
+          <Route path="/property-detail" element={<PropertyDetail />} />
+          <Route path="/general-info" element={<GeneralInfo />} />
+          <Route path="/" element={<PropertyView />}></Route>
+          <Route path="/location-info" element={<LocationInfo />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
